b1b2364da7c806d53d7ad3ac40f0acab5c15581e
armornick <nick.arnoeyts@mail.com>
2023-06-22 09:04:08 +0200

    add basic data fetching

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,39 @@
 <script>
+
+  let errorMessage = null;
+
+  const fetchLogs = async () => {
+    try {
+      const response = await fetch('/misc-log.json');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      errorMessage = error.msg || error;
+      return [];
+    }
+  };
+
+  let logsP = fetchLogs();
+
 </script>
 
 <main>
   <h1>Tracker</h1>
+  {#if errorMessage}
+    <p>
+      {errorMessage}
+    </p>
+  {/if}
+  {#await logsP}
+    <p>
+      Loading data.
+    </p>
+  {:then logs} 
+    {#each logs as log}
+      <p>{ JSON.stringify(log) }</p>
+    {/each}
+  {/await}
 </main>
 
 <style>
