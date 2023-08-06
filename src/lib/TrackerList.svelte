fc1f4444f8b479cb8b57b9194330fec8e3a7d9d0
armornick <nick.arnoeyts@mail.com>
2023-08-06 07:47:24 +0200

    add filtering by media type

--- src/lib/TrackerList.svelte
+++ src/lib/TrackerList.svelte
@@ -7,6 +7,15 @@
     /** @typedef {'earliest'|'latest'} SortOrder */
     /** @type {SortOrder} */
     let sortOrder = 'earliest';
+    /** @type {null | string} */
+    let currentFilter = null;
+
+    /** @type {string[]} */
+    const mediaTypes = (() => {
+        const /** @type {Set<string>} */ set = new Set();
+        logs.map(log => set.add(log.media));
+        return [...set];
+    })();
 
     /**
      * @param {LogRecord[]} list
@@ -22,22 +31,37 @@
         }
     }
 
-    const generateClickHandler = (/** @type {SortOrder} */ order) => {
+    const createSortHandler = (/** @type {SortOrder} */ order) => {
         return () => {
             // console.log(`clicked ${order}`);
             sortOrder = order;
         }
     };
 
-    $: sortedList = sortList(logs, sortOrder);
+    const createFilterHandler = (/** @type {string | null} */ filter) => {
+        return () => {
+            console.log(`clicked ${filter}`);
+            currentFilter = filter;
+        }
+    };
+
+    $: filteredList = currentFilter ? logs.filter(item => item.media === currentFilter) : logs;
+    $: sortedList = sortList(filteredList, sortOrder);
 </script>
 
 <section>
     <header class="action-panel">
         <p>
             <span>Sort by</span>
-            <button class="btn" on:click={generateClickHandler('latest')}>Latest</button>
-            <button class="btn" on:click={generateClickHandler('earliest')}>Earliest</button>
+            <button class="btn" on:click={createSortHandler('latest')}>Latest</button>
+            <button class="btn" on:click={createSortHandler('earliest')}>Earliest</button>
+        </p>
+        <p>
+            <span>Filter by</span>
+            <button  class="btn" on:click={createFilterHandler(null)}>Any</button>
+            {#each mediaTypes as mediaType}
+                <button class="btn" on:click={createFilterHandler(mediaType)}>{mediaType}</button>
+            {/each}
         </p>
     </header>
 
@@ -53,5 +77,9 @@
         border-radius: var(--tw-rounded);
         padding: .5rem 1rem;
         margin-block-end: 1rem;
+    }
+
+    .action-panel > p:not(:last-child) {
+        margin-bottom: .5rem;
     }
 </style>
